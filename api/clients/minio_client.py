7954d20e8c334bfe6e0757ed5318bbf891b5edfa
Samsonov Viktor Viktorovich <dezerqq26@gmail.com>
2025-05-13 03:59:27 +0300

    [TASK-3] feat: base tests + ci

--- api/clients/minio_client.py
+++ api/clients/minio_client.py
@@ -2,9 +2,10 @@ from contextlib import asynccontextmanager
 from aiobotocore.session import get_session
 from config import settings
 
+
 class MinioClient:
     def __init__(
-        self, access_key: str, secret_key: str, bucket_name: str, endpoint_url: str
+            self, access_key: str, secret_key: str, bucket_name: str, endpoint_url: str
     ):
         self.config = {
             "aws_access_key_id": access_key,
