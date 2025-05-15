0ddb2251d288df73a5a7c32cdb23c1944b85020a
Max Pogrebnykh <maksim.pogrebnykh@glowbyteconsulting.com>
2025-05-15 20:03:23 +0300

    web socket; workflow <--redis--> api;

---
+++ readme.md
@@ -0,0 +1,15 @@
+# readme
+
+Для запуска приложения выполнить следующую команду из корня приложения:
+
+```shell
+docker compose up --build
+```
+
+p.s. убедиться, что docker daemon is active;
+
+Доступны следующие интерфейсы на `localhost`:
+
+- Swagger :8000
+- Temporal :8080
+- Minio :9001 (minioadmin/minioadmin)
--- worker/config.py
+++ worker/config.py
@@ -1,13 +1,6 @@
 import os
+
 from pydantic import BaseModel
-
-
-class DBConfig(BaseModel):
-    db_name: str
-    db_user: str
-    db_password: str
-    db_host: str
-    db_port: int
 
 
 class MinioConfig(BaseModel):
@@ -23,20 +16,12 @@ class RedisConfig(BaseModel):
 
 
 class Settings(BaseModel):
-    db: DBConfig
     minio: MinioConfig
     redis: RedisConfig
     DATABASE_URL: str
 
 
 settings = Settings(
-    db=DBConfig(
-        db_name=os.environ.get("POSTGRES_DB"),
-        db_user=os.environ.get("POSTGRES_USER"),
-        db_password=os.environ.get("POSTGRES_PASSWORD"),
-        db_host=os.environ.get("POSTGRES_HOST"),
-        db_port=int(os.environ.get("POSTGRES_PORT")),
-    ),
     minio=MinioConfig(
         access_key=os.environ.get("MINIO_ACCESS_KEY"),
         secret_key=os.environ.get("MINIO_SECRET_KEY"),
