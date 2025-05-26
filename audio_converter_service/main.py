da4ea7b26a2e1eacac7ae8d41736f2f1ebf0b0ea
Artemiy Renev <166447502+otterEva@users.noreply.github.com>
2025-05-26 04:33:23 +0300

    updated

--- audio_converter_service/main.py
+++ audio_converter_service/main.py
@@ -1,12 +1,10 @@
 from fastapi import FastAPI, WebSocket
-import httpx
+import model
 
 app = FastAPI()
 
 @app.websocket("/ws")
 async def websocket_endpoint(websocket: WebSocket):
     await websocket.accept()
-    url_to_download = await websocket.receive_text()
-
-    async with httpx.AsyncClient() as client:
-        resp = await client.get(f"http://app/{url_to_download}")
+    file = model.audio_converter()
+    return file
