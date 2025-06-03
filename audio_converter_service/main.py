bed15e8521de1546e2a6e95cd4f7ff859ef7e783
Max Pogrebnykh <maksim.pogrebnykh@glowbyteconsulting.com>
2025-06-03 15:35:08 +0300

    feat: audio conv.;

--- audio_converter_service/main.py
+++ audio_converter_service/main.py
@@ -1,7 +1,6 @@
 import model
 
 from fastapi import FastAPI
-from uuid import uuid4
 
 from file_service import file_service
 
@@ -14,9 +13,15 @@ async def convert_file(file_path: str):
     # 1. get file original from minio
     file_bytes = await file_service.get_file(filename=file_path)
     # 2. convert file -- model
-    converted_file: bytes = await model.audio_converter(file_bytes)
+    file_name = file_path[:file_path.rfind(".")]
+    file_extension = file_path[file_path.rfind("."):]
+
+    converted_file: bytes = await model.audio_converter(
+        file_bytes=file_bytes,
+        file_extension=file_extension
+    )
     # 3. post converted file to minio
     converted_file_name = await file_service.upload_file(
-        file=converted_file, filename=f"converted_{uuid4()}.mp4"
+        file=converted_file, filename=f"converted_{file_name}.wav"
     )
     return converted_file_name
--- audio_converter_service/model.py
+++ audio_converter_service/model.py
@@ -1,2 +1,35 @@
-async def audio_converter(file):
-    return b"aboba"
+import subprocess
+import tempfile
+import os
+
+
+async def audio_converter(
+    file_bytes: bytes,
+    file_extension: str = ".wav"
+) -> bytes | None:
+    with tempfile.NamedTemporaryFile(suffix=file_extension, delete=False) as tmp_input:
+        tmp_input.write(file_bytes)
+        tmp_input_path = tmp_input.name
+
+    with tempfile.NamedTemporaryFile(suffix=".wav", delete=False) as tmp_output:
+        tmp_output_path = tmp_output.name
+
+    try:
+        cmd = [
+            "ffmpeg",
+            "-y",
+            "-i", tmp_input_path,
+            "-ar", "16000",
+            "-ac", "1",
+            tmp_output_path
+        ]
+
+        subprocess.run(cmd, check=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+
+        with open(tmp_output_path, "rb") as f:
+            output_bytes = f.read()
+
+        return output_bytes
+    finally:
+        os.remove(tmp_input_path)
+        os.remove(tmp_output_path)
