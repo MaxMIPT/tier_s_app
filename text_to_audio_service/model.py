fd402ee099d345f939bbb040c5eca2e3301a90d6
Кирилл Едрышов <edkirill@yandex.ru>
2025-06-04 01:46:02 +0300

    fix mp

--- text_to_audio_service/model.py
+++ text_to_audio_service/model.py
@@ -7,6 +7,17 @@ import tempfile
 from transliterate import translit
 from pathlib import Path
 
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+print(device)
+
+model, _ = torch.hub.load(
+    repo_or_dir='snakers4/silero-models',
+    model='silero_tts',
+    language='ru',
+    speaker='v4_ru',
+    trust_repo=True
+)
+
 # Функция для транслитерации английских слов
 def transliterate_en_words(text_):
     return re.sub(r'\b[a-zA-Z]+\b', lambda m: translit(m.group(), 'ru'), text_)
@@ -14,19 +25,6 @@ def transliterate_en_words(text_):
 def text_to_audio(text_: str):
     processed_text = transliterate_en_words(text_)
     print("Текст после транслитерации:", processed_text)
-
-    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-
-    # Загружаем модель
-    model, _ = torch.hub.load(
-        repo_or_dir='snakers4/silero-models',
-        model='silero_tts',
-        language='ru',
-        speaker='v4_ru',
-        trust_repo=True
-    )
-
-    model = model.to(device)
 
     # Синтез речи
     audio = model.apply_tts(
