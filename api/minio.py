4a89ca985ed396fa1584606236f4531ea0b6fbde
Max Pogrebnykh <maksim.pogrebnykh@glowbyteconsulting.com>
2025-06-04 15:12:28 +0300

    minio policy;

--- api/minio.py
+++ api/minio.py
@@ -1,3 +1,5 @@
+import json
+
 from aiobotocore.session import get_session
 
 from config import settings
@@ -13,5 +15,22 @@ async def create_bucket(bucket_name: str):
     ) as s3:
         try:
             await s3.create_bucket(Bucket=bucket_name)
+            policy = {
+                "Version": "2012-10-17",
+                "Statement": [
+                    {
+                        "Effect": "Allow",
+                        "Principal": {
+                            "AWS": ["*"]
+                        },
+                        "Action": ["s3:GetObject"],
+                        "Resource": [f"arn:aws:s3:::{bucket_name}/*"]
+                    }
+                ]
+            }
+            await s3.put_bucket_policy(
+                Bucket=bucket_name,
+                Policy=json.dumps(policy)
+            )
         except Exception as e:  # noqa
             pass
